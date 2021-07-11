7f6d98c28f5a0acb40040e603054529687b5e4e7
hjpettegrove <hjpettegrove@gmail.com>
2021-07-11 10:54:35 -0700

    add test route

--- src/routes/wallet.js
+++ src/routes/wallet.js
@@ -4,6 +4,8 @@ import wallet_controller from '../controllers/wallet.js';
 
 const router = Router();
 
+router.get('/test/:network/address/:address/balance', wallet_controller.test_balance);
+
 router.get('/:network/address/:address/balance', wallet_controller.wallet_balance);
 
 export default router;

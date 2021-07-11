02f1322d09ac9236a813a0ce8076c3fc6cc577e7
hjpettegrove <hjpettegrove@gmail.com>
2021-07-11 11:22:42 -0700

    update cachedFetch

--- src/helpers/cachedFetch.js
+++ src/helpers/cachedFetch.js
@@ -1,43 +1,43 @@
-import 'dotenv/config';
-
-import fetch from 'node-fetch';
-import path from 'path';
-import flatcache from 'flat-cache';
-
 //  Basing off the ideas found here:
 //  https://www.npmjs.com/package/flat-cache
 //  https://scotch.io/tutorials/how-to-optimize-node-requests-with-simple-caching-strategies
 //  https://www.sitepoint.com/cache-fetched-ajax-requests/
 //  https://stackabuse.com/reading-and-writing-json-files-with-node-js -- potentiallu replaces flat-cache?
 
+import fetch from 'node-fetch';
+import path from 'path';
+import flatcache from 'flat-cache';
 
 
 const cachedFetch = (url, options) => {
     let cacheKey = url;
-    let cached = flatcache.load('zkswapCache', path.resolve('./tmp/cache'))
-    let cachedContent = cached.getKey(cacheKey);
-    if(cachedContent) {
-        //  Already cached
-        let response = new Response(new Blob([cachedContent]));
-        return Promise.resolve(response);
+    let cache = flatcache.load('cacheId')
+    let cachedContent = cache.getKey(cacheKey)
+    if(cachedContent != null)
+    {
+        console.log(`cache found for key ${cacheKey}`)
+        let response = new Response(new Blob([cachedContent]))
+        return Promise.resolve(response)
     }
 
-    //  Cache doesnt exist, make request
-
-    return fetch(url, options).then(resp => {
-        // let's only store in cache if the content-type is
-        // JSON or something non-binary
-        if(resp.status === 200) {
-            let ct = resp.headers.get('Content-Type')
-            if(ct && (ct.match(/application\/json/i) || ct.match(/text\//i))) {
-                resp.clone().text().then(content => {
-                    cached.setKey(cacheKey, content)
+    return fetch(url, options).then(response => {
+        //  Only cache non binary content
+        if(response.status === 200)
+        {
+            console.log(`positive response status 200`)
+            let ctx = response.headers.get('Content-Type')
+            console.log(ctx)
+            if(ctx && (ctx.match(/application\/json/i) || ctx.match(/text\//i)))
+            {
+                console.log('content type was text or json')
+                response.clone().text().then(content => {
+                    console.log(content)
+                    cache.setKey(cacheKey, content)
                 })
             }
         }
-        return resp;
+        return response
     })
-
 }
 
-export default { cachedFetch }
+export default cachedFetch

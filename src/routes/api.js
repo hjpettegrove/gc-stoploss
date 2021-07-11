dfb28af95444f8bbcb56550070deb95d8f63bd71
hjpettegrove <hjpettegrove@gmail.com>
2021-07-11 10:54:23 -0700

    removed unused routes

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,13 +1,9 @@
 import { Router } from 'express';
-import token from './tokens.js';
-import pairs from './pairs.js';
 import account from './account.js';
 import wallet from './wallet.js'
 
 const router = Router();
 
-router.use('/tokens', token);
-router.use('/pairs', pairs);
 router.use('/account', account);
 router.use('/wallet', wallet);
 

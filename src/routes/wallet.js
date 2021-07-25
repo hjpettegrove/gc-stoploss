05590d7dd25cf7f35b53b454acbbab12b1e3c12d
hjpettegrove <hjpettegrove@gmail.com>
2021-07-25 14:49:46 -0700

    removed old wallet controller

--- src/routes/wallet.js
+++ src/routes/wallet.js
@@ -8,8 +8,4 @@ const router = Router();
 
 router.get('/balance/:network/:address', controller.getWalletBalance)
 
-//router.get('/test/:network/address/:address/balance', wallet_controller.test_balance);
-
-//router.get('/:network/address/:address/balance', wallet_controller.wallet_balance);
-
 export default router;

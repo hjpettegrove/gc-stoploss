d854a822e9060a8d59c665b7003bcd2816f59e7d
hjpettegrove <hjpettegrove@gmail.com>
2021-09-26 14:37:52 -0700

    updated api route

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,10 +1,9 @@
 import { Router } from 'express';
-import account from './account.js';
+
 import wallet from './wallet.js'
 
 const router = Router();
 
-router.use('/account', account);
 router.use('/wallet', wallet);
 
 export default router;

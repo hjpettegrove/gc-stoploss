e16120418d3ddf9f0b7ad2bae2924933a396cd14
hjpettegrove <hjpettegrove@gmail.com>
2021-09-26 14:38:03 -0700

    formatting

--- src/routes/wallet.js
+++ src/routes/wallet.js
@@ -2,8 +2,6 @@ import { Router } from 'express';
 
 import controller from '../controllers/walletController.js'
 
-
-
 const router = Router();
 
 router.get('/balance/:network/:address', controller.getWalletBalance)

bca1d13a10f6e773b14243e6ecd4a2d5781b2d05
hjpettegrove <hjpettegrove@gmail.com>
2021-09-26 14:37:37 -0700

    removed old url route creators

--- src/services/zkswap-api.js
+++
@@ -1,8 +0,0 @@
-import zksEpnt from './zksync-endpoints.js';
-import fetch from 'node-fetch';
-
-
-const zksTokenList = async (networkId) => {
-    const network = (networkId > 0 ? networkId : 1)
-    
-}
